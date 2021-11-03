e7c2a0513f66c46db8186325801cea5da7c27aae
trazumov <trazumov@list.ru>
2021-11-03 15:12:40 +0300

    просто закомментировал строку с парсером

--- src/main.c
+++ src/main.c
@@ -8,7 +8,6 @@ int	main(int argc, char **argv, char **envp)
 	shell.exit = FALSE;
 	while (shell.exit != TRUE)
 	{
-		//printf("minishell >:");
 		init_env(&shell, envp);
 		/*
 		Обработка вводимых данных
