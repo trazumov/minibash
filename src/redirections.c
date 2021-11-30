4475eb5c43ffc8dd7c5df37d54253dd0959c6a2c
trazumov <trazumov@list.ru>
2021-12-01 02:48:46 +0300

    Рефактор редиректов

--- src/redirections.c
+++ src/redirections.c
@@ -20,21 +20,6 @@ static t_token *get_prev_input(t_token *token)
 	return (res);
 }
 
-// redir_type 0 is INPUT, redir_type 1 is OUTPUT
-static void close_prev_redir(int redir_type, int *redir_val, t_token *token, t_minishell *shell)
-{
-	if (redir_type == 0 && *redir_val > 0)
-	{
-		get_prev_input(token)->skip = TRUE;
-		close(shell->fd_in);
-	}
-	if (redir_type == 1 && *redir_val > 0)
-	{
-		get_prev_redir(token)->skip = TRUE;
-		close(shell->fd_out);
-	}
-}
-
 // return 0 if OK ; return 1 if ERROR
 static int redirect_out(t_minishell *shell, t_token *token, int *new_output)
 {
@@ -59,9 +44,50 @@ static int redirect_out(t_minishell *shell, t_token *token, int *new_output)
 	return (0);
 }
 
+static int redirect_in(t_minishell *shell, t_token *token, int *new_input)
+{
+	if ((*new_input) == TRUE)
+	{
+		get_prev_input(token)->skip = TRUE;
+		close(shell->fd_in);
+	}
+	if (token->next)
+	{
+		if (token->type == REDIR_IN)
+			shell->fd_in = open(token->next->str, O_RDONLY, S_IRWXU);
+		dup2(shell->fd_in, STDIN);
+		(*new_input) = TRUE;
+	}
+	else
+		(*new_input) = -1;
+	if (!(*new_input))
+		return (1);
+	return (0);
+}
+
+static int redirect_heredoc(t_minishell *shell, t_token *token, int *new_input)
+{
+	if ((*new_input) == TRUE)
+	{
+		get_prev_input(token)->skip = TRUE;
+		close(shell->fd_in);
+	}
+	if (token->next)
+	{
+		if (token->type == REDIR_HEREDOC)
+			exec_here_doc(shell, token);
+		(*new_input) = TRUE;
+	}
+	else
+		(*new_input) = -1;
+	if (!(*new_input))
+		return (1);
+	return (0);
+}
+
 int set_redirection(t_minishell *shell)
 {
-	t_token *token;
+	t_token	*token;
 	int		new_input;
 	int		new_output;
 
@@ -71,49 +97,14 @@ int set_redirection(t_minishell *shell)
 	while (token)
 	{
 		if (token->type == REDIR_OUT || token->type == REDIR_OUT_2)
-		{
 			if (redirect_out(shell, token, &new_output))
 				return (open_file_error(shell));
-		}
-		//----------
 		if (token->type == REDIR_IN)
-		{
-			if (new_input == TRUE)
-			{
-				get_prev_input(token)->skip = TRUE;
-				close(shell->fd_in);
-			}
-			if (token->next)
-			{
-				if (token->type == REDIR_IN)
-					shell->fd_in = open(token->next->str, O_RDONLY, S_IRWXU);
-				dup2(shell->fd_in, STDIN);
-				new_input = TRUE;
-			}
-			else
-				new_input = -1;
-			if (!new_input)
+			if (redirect_in(shell, token, &new_input))
 				return (open_file_error(shell));
-		}
-		//
 		if (token->type == REDIR_HEREDOC)
-		{
-			if (new_input == TRUE)
-			{
-				get_prev_input(token)->skip = TRUE;
-				close(shell->fd_in);
-			}
-			if (token->next)
-			{
-				if (token->type == REDIR_HEREDOC)
-					exec_here_doc(shell, token);
-				new_input = TRUE;
-			}
-			else
-				new_input = -1;
-			if (!new_input)
+			if (redirect_heredoc(shell, token, &new_input))	
 				return (open_file_error(shell));
-		}
 		token = token->next;
 	}
 	return (0);
