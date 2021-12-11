98d2b803ab53415e4e7053ae0dc94e0a66b64af2
ibariev <ilgiz-gek@mail.ru>
2021-12-11 12:46:01 +0300

    Исправил ft_atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mlatashi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mlatashi <mlatashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 18:26:15 by mlatashi          #+#    #+#             */
-/*   Updated: 2021/04/27 18:30:43 by mlatashi         ###   ########.fr       */
+/*   Updated: 2021/12/11 12:40:02 by mlatashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	ft_atoi(const char *str)
 	}
 	else if (str[i] == '+')
 		i++;
-	else if (str[i] < 48 && str[i] > 57)
+	else if (str[i] < 48 || str[i] > 57)
 		return (0);
 	while (str[i] >= 48 && str[i] <= 57 && str[i] != '\0')
 	{
