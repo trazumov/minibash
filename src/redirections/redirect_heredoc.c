431d39b8a9f8d8d0b2e19e740665d0889060a860
trazumov <trazumov@list.ru>
2021-12-16 10:54:39 +0300

    Вместо exit_shell_err void_shell_err который не выходит из минишелла

--- src/redirections/redirect_heredoc.c
+++ src/redirections/redirect_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: svirgil <svirgil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 18:47:20 by svirgil           #+#    #+#             */
-/*   Updated: 2021/12/15 12:56:26 by svirgil          ###   ########.fr       */
+/*   Updated: 2021/12/16 10:53:36 by svirgil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,9 +56,9 @@ t_minishell *shell, pid_t parent)
 	{
 		shell->fd_in = open("here_doc", O_RDONLY);
 		if (shell->fd_in < 0 || read(shell->fd_in, 0, 0) < 0)
-			exit_shell_err(shell);
+			void_shell_err(shell);
 		else if (dup2(shell->fd_in, 0) == -1)
-			exit_shell_err(shell);
+			void_shell_err(shell);
 		close_fd_save(shell->fd_in);
 	}
 }
